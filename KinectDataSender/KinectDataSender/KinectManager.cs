44f42a86800ca9e880917586edaa728e07182f24
asahiufo <asahiufo@ahiufomasao.org>
2012-09-27 01:38:40 +0900

    軽く画面デザインした

---
+++ KinectDataSender/KinectDataSender/KinectManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace KinectDataSender
+{
+    /// <summary>
+    /// Kinectマネージャー
+    /// </summary>
+    class KinectManager
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public KinectManager()
+        {
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public void Initialize()
+        {
+            // Kinectが接続されていない場合
+            if (KinectSensor.KinectSensors.Count == 0)
+            {
+                throw new Exception("Kinectを接続して下さい。");
+            }
+        }
+    }
+}
